f066c394a6025aa36459c0a60cfdb292fc2355e2
Angius <koumarin@gmail.com>
2017-12-04 21:45:43 +0100

    Updated to Mahapps.Metro

--- YTViewer/MainWindow.xaml.cs
+++ YTViewer/MainWindow.xaml.cs
@@ -12,17 +12,29 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace YTViewer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void App_Activated(object sender, EventArgs e)
+        {
+            Topmost = true;
+        }
+
+        void App_Deactivated(object sender, EventArgs e)
+        {
+            Topmost = true;
+            Activate();
         }
 
         private void Load_Btn_Click(object sender, RoutedEventArgs e)
@@ -30,6 +42,32 @@ namespace YTViewer
             string url = "https://www.youtube.com/embed/" + videoID_TxtBx.Text + "?fs=0&iv-load-policy=3&rel=0&modestbranding=1";
             Video_WB.Address = url;
             debug.Text = " https://www.youtube.com/watch?v=" + videoID_TxtBx.Text;
+        }
+
+        bool menuFlyoutFlipFlop = false;
+        private void Settings_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!menuFlyoutFlipFlop)
+            {
+                Menu_Flyout.IsOpen = true;
+                menuFlyoutFlipFlop = true;
+            }
+            else
+            {
+                Menu_Flyout.IsOpen = false;
+                menuFlyoutFlipFlop = false;
+            }
+            
+        }
+
+        private void Settings_Btn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Settings_Ico.Spin = true;
+        }
+
+        private void Settings_Btn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Settings_Ico.Spin = false;
         }
     }
 }
